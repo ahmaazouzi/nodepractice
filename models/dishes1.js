0ac30f3b6649c3f3869e62b69162fc12f5a8c1d4
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2016-05-21 12:34:11 -0400

    Mongoose

--- models/dishes1.js
+++ models/dishes1.js
@@ -1,20 +1,57 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+//create Comment schema
+var CommentSchema = new Schema({
+	rating: {
+		type: Number,
+		min: 1,
+		max: 5,
+		required: true
+	},
+	comment: {
+		type: String,
+		required: true
+	},
+	author: {
+		type: String,
+		required: true
+	}
+}, {
+	timestamps: true
+});
 
 //create schema
 var dishSchema = new Schema({
 	name: {
 		type: String,
-		requireed: true,
+		required: true,
 		unique: true
 	},
 	description: {
 		type: String,
-		requireed: true
-	}
+		required: true
+	},
+	image: {
+		type: String
+	},
+	category: {
+		type: String
+	},
+	label: {
+		type: String
+	},
+	price: {
+		type: Currency
+	},
+	comments: [CommentSchema]
 },
 	{
-		timestamps: true
+		timestamps: true,
+		comments: true,
+		zaza: true
 });
 
 //create model
--- routes/index.js
+++ routes/index.js
@@ -50,7 +50,8 @@ router.route('/:dishId')
 		$set: req.body
 	}, {
 		new: true
-	}, function(err, dish){
+	}, function(err, dish
+		){
 		if (err) throw err;
 		res.json(dish);
 	});
@@ -60,6 +61,82 @@ router.route('/:dishId')
 	Dishes.remove(req.params.dishId, function(err, resp){
 		if (err) throw err;
 		res.json(resp);
+	});
+});
+
+//handling comments
+router.route('/:dishId/comments')
+.get(function(req, res, next){
+	Dishes.findById(req.params.dishId, function(err, dish){
+		if (err) throw err;
+		res.json(dish.comments);
+	});
+})
+
+.post(function(req, res, next){
+	Dishes.findById(req.params.dishId, function(err, dish){
+		if (err) throw err;
+		dish.comments.push(req.body);
+
+		dish.save(function(err, dish){
+			if (err) throw err;
+			console.log('Updated comments');
+
+			res.json(dish);
+		});
+	});
+})
+
+.delete(function(req, res, next){
+	if (err) throw err;
+
+	for (var i = (dish.comments.length - 1); i >= 0; i--){
+		dish.comments.id(dish.comments[i]._id).remove();
+	}
+
+	dish.save(function(err, result){
+		if (err) throw err;
+		res.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
+		res.end('Deleted all comments!!');
+	});
+	
+});
+
+//specific comments
+router.route('/:dishId/comments/:commentId')
+.get(function(req, res, next){
+	Dishes.findById(req.params.dishId, function(err, dish){
+		if (err) throw err;
+		res.json(dish.comments.id(req.params.commentId));
+	});
+})
+
+.put(function(req, res, next){
+	Dishes.findById(req.params.dishId, function(err, dish){
+		if (err) throw err;
+
+		dish.comments.id(req.params.commentId).remove();
+
+		dish.comments.push(req.body);
+
+		dish.save(function(err, dish){
+			if (err) throw err;
+			console.log('Updated comment!');
+			console.log(dish);
+		});
+	});
+})
+
+.delete(function(req, res, next){
+	Dishes.findById(req.params.dishId, function(err, dish){
+		dish.comments.id(req.params.commentId).remove();
+
+		dish.save(function(err, resp){
+			if (err) throw err;
+			res.json(resp);
+		});
 	});
 });
 
